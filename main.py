a56348974cb6cffe04ec764bca8dcd8806a22f4f
Rick <fuck@off.lol>
2019-04-29 09:47:12 -0400

    client and server

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-from p2p import setup
+import p2p
 
-setup('127.0.0.1', 8888, interface="lo")
+client = p2p.create()
+
+host = p2p.getIp('www.google.com');
+port = 80;
+
+p2p.connect(client, host, port)
+
+p2p.send(client, "GET / HTTP/1.1\r\n\r\n")
+p2p.receive(client)
+
+HOST = ''	# Symbolic name meaning all available interfaces
+PORT = 8888	# Arbitrary non-privileged port
+
+server = p2p.create()
+p2p.bind(server, HOST, PORT)
+p2p.listen(server)
+p2p.startThreadedServer(server)
--- p2p/__init__.py
+++ p2p/__init__.py
@@ -1,18 +1,85 @@
-from pyp2p.net import *
-import time
+import socket	#for sockets
+import sys	#for exit
+from thread import *
 
-#Setup Alice's p2p node.
-def setup(ip, port, interface="eth0:2", node_type="passive", debug=1 ):
-    alice = Net(passive_bind=ip, passive_port=port, interface=interface, node_type=node_type, debug=debug)
-    alice.start()
-    alice.bootstrap()
-    alice.advertise()
-    #Event loop.
+def create():
+    #create an INET, STREAMing socket
+    try:
+	       s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+	       print 'Failed to create socket'
+	       sys.exit()
+    print('Socket Created')
+    return s
+
+def bind(s, host, port):
+    try:
+    	s.bind((host, port))
+    except socket.error , msg:
+    	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    	sys.exit()
+    print 'Socket bind complete'
+
+def listen(s):
+    s.listen(10)
+    print 'Socket now listening'
+
+def accept(s):
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+def clientthread(conn):
+	#Sending message to connected client
+	conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+	#infinite loop so that function do not terminate and thread do not end.
+	while True:
+		#Receiving from client
+		data = conn.recv(1024)
+		reply = 'OK...' + data
+		if not data:
+			break
+		conn.sendall(reply)
+	conn.close()
+
+def startThreadedServer(s):
     while 1:
-        for con in alice:
-            for reply in con:
-                print(reply)
-        time.sleep(1)
+        #wait to accept a connection - blocking call
+    	conn, addr = s.accept()
+    	print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    	#start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    	start_new_thread(clientthread ,(conn,))
 
-def joke():
-    return ('Why\'d the chicken cross the road')
+def getIp(host):
+    try:
+	remote_ip = socket.gethostbyname( host )
+
+    except socket.gaierror:
+	#could not resolve
+	print 'Hostname could not be resolved. Exiting'
+	sys.exit()
+    return remote_ip
+
+def connect(s, remote_ip, port):
+    #Connect to remote server
+    s.connect((remote_ip , port))
+    print('Socket Connected to ' + str(remote_ip)  + ':' + str(port))
+
+def send(s, message):
+    try :
+	#Set the whole string
+	s.sendall(message)
+    except socket.error:
+	#Send failed
+	print 'Send failed'
+	sys.exit()
+    print 'Message send successfully'
+
+def receive(s):
+    #Now receive data
+    reply = s.recv(4096)
+    print(reply)
+    return reply
+
+def close(s):
+    print "Closing socket connection"
+    s.close()
